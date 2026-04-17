0d7a3427d78312bbd6b465442b60c0987233e5dd
cm4cm4 <korawitlongjun452@gmail.com>
2026-04-17 22:07:25 +0700

    changes:  - added functionality for deleting tasks  - added functionality for addding tasks and seeing them  - added functionality to cross out tasks altough it doesnt update in localstorage i will fix it soon

--- src/script.ts
+++ src/script.ts
@@ -4,9 +4,55 @@ const addTaskButton = document.getElementById("addTaskButton")
 const taskName = document.getElementById("taskName") as HTMLInputElement
 const taskListElement = document.getElementById("todoList")
 
+for (const task of JSON.parse(localStorage.getItem("tasks") || "[]")){
+    createTaskElement(task)
+}
+
+
+
 function createTaskElement(task: Task): void{
-    const listElement = document.createElement("li")
-    listElement.classList.add("todoItem", task.Task)
+    const todoItem = document.createElement("li")
+    todoItem.className = "todoItem"
+    todoItem.id = String(task.Id)
+    
+    const todoTextZone = document.createElement("div")
+    todoTextZone.className = "todoTextZone"
+
+    const todoCheckBox = document.createElement("input")
+    todoCheckBox.type = "checkbox"
+    todoCheckBox.className = "todoCheckBox"
+
+    const todoText = document.createElement("p")
+    todoText.className = "todoText"
+    todoText.append(task.Task)
+
+    todoTextZone.appendChild(todoCheckBox)
+    todoTextZone.appendChild(todoText)
+
+    const todoDeleteButton = document.createElement("todoDeleteButton")
+    todoDeleteButton.className = "todoDeleteButton"
+    todoDeleteButton.append("Delete")
+
+    todoItem.appendChild(todoTextZone)
+    todoItem.appendChild(todoDeleteButton)
+
+    taskListElement?.appendChild(todoItem)
+    
+    todoCheckBox.addEventListener("change", (e) => {
+        if (e.currentTarget?.checked){
+            todoText.classList.add("todoComplete")
+        }
+        else{
+            todoText.classList.remove("todoComplete")
+        }
+    })
+
+    todoDeleteButton.addEventListener("click", (e) => {
+        todoItem.remove()
+        const tasks: TaskList = JSON.parse(localStorage.getItem("tasks") || "[]")
+        localStorage.setItem("tasks",JSON.stringify(tasks.filter((item) => item.Id == task.Id)))
+    })
+    console.log("im also ran")
 }
 
 
@@ -21,6 +67,7 @@ function addTask(task: Task): void{
     tasksList.push(task)
     console.log("im ran")
     localStorage.setItem("tasks", JSON.stringify(tasksList))
+    createTaskElement(task)
 
 }
 
@@ -33,3 +80,4 @@ addTaskButton?.addEventListener("click", function(e){
     }
     addTask(newTask)
 })
+
