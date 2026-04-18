9a7a5b5836a182ee322a3a3aa9299567e82690ba
cm4cm4 <korawitlongjun452@gmail.com>
2026-04-18 21:13:11 +0700

    created build files

---
+++ build.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/env fish
+
+sudo apt install npm
+
+npm install
+
+npx tsc
+npx @tailwindcss/cli -i ./src/style.css -o ./src/tailwindstyle.css
+
+mkdir dist
+
+cp src/index.html dist/
+cp -r ./src/outTS dist/
+cp src/tailwindstyle.css dist/
